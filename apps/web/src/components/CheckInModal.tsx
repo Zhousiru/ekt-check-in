4bfccd0232c5750fbc3e1abe6cd7df3eb4d3391a
Zhousiru <i@zhousiru.com>
2023-06-13 20:20:25 +0800

    feat: title, info, favicon

--- apps/web/src/components/CheckInModal.tsx
+++ apps/web/src/components/CheckInModal.tsx
@@ -1,8 +1,14 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
   Button,
   Checkbox,
   Code,
+  Flex,
   Input,
   InputGroup,
   InputRightElement,
@@ -46,54 +52,66 @@ export function CheckInModal({
         <ModalCloseButton />
 
         <ModalBody>
-          <Checkbox
-            colorScheme="teal"
-            checked={noVpn}
-            onChange={() => setNoVpn((prev) => !prev)}
-          >
-            我可以访问 <Code>ekt.cuit.edu.cn</Code>
-          </Checkbox>
+          <Flex gap={4} direction="column">
+            <Alert status="info">
+              <AlertIcon />
+              <Box>
+                <AlertTitle>提示</AlertTitle>
+                <AlertDescription>
+                  签退按钮在签到按钮的下方，与背景色相同
+                </AlertDescription>
+              </Box>
+            </Alert>
 
-          <OrderedList my={4}>
-            <ListItem>
-              <Text>
-                确保你已经
-                <Link
-                  ml={1}
-                  href={
-                    noVpn
-                      ? "//ekt-cuit-edu-cn.webvpn.cuit.edu.cn:8118/api/mSsoLogin"
-                      : "//ekt.cuit.edu.cn"
-                  }
-                  color="teal"
-                  isExternal
+            <Checkbox
+              colorScheme="teal"
+              checked={noVpn}
+              onChange={() => setNoVpn((prev) => !prev)}
+            >
+              我可以访问 <Code>ekt.cuit.edu.cn</Code>
+            </Checkbox>
+
+            <OrderedList>
+              <ListItem>
+                <Text>
+                  确保你已经
+                  <Link
+                    ml={1}
+                    href={
+                      noVpn
+                        ? "//ekt-cuit-edu-cn.webvpn.cuit.edu.cn:8118/api/mSsoLogin"
+                        : "//ekt.cuit.edu.cn"
+                    }
+                    color="teal"
+                    isExternal
+                  >
+                    登陆第二课堂
+                    <ExternalLinkIcon ml={1} />
+                  </Link>
+                </Text>
+              </ListItem>
+
+              <ListItem>打开或复制签到链接</ListItem>
+            </OrderedList>
+
+            <InputGroup>
+              <Input
+                pr="4.5rem"
+                placeholder="Enter password"
+                readOnly={true}
+                value={link}
+              />
+              <InputRightElement width="4rem">
+                <Button
+                  h="1.75rem"
+                  size="sm"
+                  onClick={() => window.open(link, "_blank")}
                 >
-                  登陆第二课堂
-                  <ExternalLinkIcon ml={1} />
-                </Link>
-              </Text>
-            </ListItem>
-
-            <ListItem>打开或复制签到链接</ListItem>
-          </OrderedList>
-
-          <InputGroup>
-            <Input
-              pr="4.5rem"
-              placeholder="Enter password"
-              readOnly={true}
-              value={link}
-            />
-            <InputRightElement width="4rem">
-              <Button
-                h="1.75rem"
-                size="sm"
-                onClick={() => window.open(link, "_blank")}
-              >
-                打开
-              </Button>
-            </InputRightElement>
-          </InputGroup>
+                  打开
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </Flex>
         </ModalBody>
 
         <ModalFooter>
